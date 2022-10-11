d958c59840590367c0a9f83ba91fdf1b4d6e500f
Praba <prama1303@gmail.com>
2022-10-11 22:22:46 +0200

    Table Searching with better eff. search function

--- client/src/components/TableSearch.js
+++ client/src/components/TableSearch.js
@@ -6,12 +6,25 @@ const TableSearch = () => {
   const [query, setQuery] = useState('');
   //console.log(query);
 
+  const keys = ['first_name', 'last_name', 'email'];
+
+  //console.log(Users[0]);
+  //console.log(Users[0]['first_name']);
+
+  /*  
   const search = (data) => {
     return data.filter(
       (item) =>
         item.first_name.toLowerCase().includes(query) ||
         item.last_name.toLowerCase().includes(query) ||
         item.email.toLowerCase().includes(query)
+    );
+  };
+*/
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
     );
   };
 
@@ -21,7 +34,8 @@ const TableSearch = () => {
         type='text'
         className='search'
         placeholder='Search...'
-        onChange={(e) => setQuery(e.target.value)}
+        //onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
       {/** <Table data={Users} />*/}
       <Table data={search(Users)} />
